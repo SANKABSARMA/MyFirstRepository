2b76a60b5b96b5ae6f3c068025126f161d7041c5
SANKABSARMA <sankabsharma12@gmail.com>
2016-12-27 20:57:59 +0530

    first commit

---
+++ Code/src/prigram20/Prigram20.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prigram20;
+import java.io.*;
+/**
+ *
+ * @author Admin
+ */
+public class Prigram20 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+
+String dirname="D:/sankab/sarma";
+File f=new File(dirname);
+f.mkdirs();
+  }
+
+        // TODO code application logic here
+    
+    
+}
